0e7492cc755ce9c8cd86222977826502865a44be
Carrie Cloninger <carrie.cloninger@umontana.edu>
2018-02-06 12:30:30 -0700

    Part 1 HW3
    Practiced setting up a canvas, background color, and text using p5.js

---
+++ HW3/01_Hello_World/sketch.js
@@ -0,0 +1,14 @@
+// declare the setup function
+function setup() {
+  // creation of canvas with parameters: 600 px wide and 400 px high
+  createCanvas(600, 400);
+  // choose background color for newly created canvas
+  background('green');
+}
+
+// declare the draw function
+function draw() {
+  // use 'text()' function to print string: Hello World!
+  // include parameters for string position on canvas
+  text("Hello World!", 20, 30);
+}
