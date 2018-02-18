ba0b9b110472475e65b87fed64de84d91b54b6e5
Carrie Cloninger <carrie.cloninger@umontana.edu>
2018-02-18 14:27:59 -0700

    HW4 Check #1

---
+++ HW4/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(1000, 700);
+}
+
+function draw() {
+  background('rgb(153, 255, 153)');
+
+  // creating the MAIN SANDBOX for entire character
+  push();
+
+  // move XY coordinate grid to center of createCanvas
+  translate(500, 300);
+
+  // drawing legs - there are four total
+  push();
+  fill('rgb(135, 248, 107)');
+  // draw leftmost leg
+  quad(-160, -20, -120, -20, -180, -120, -220, -120);
+  quad(-220, -120, -180, -120, -160, -180, -160, -280);
+  // draw left, inside leg
+  quad(-80, -40, -40, -40, -60, -100, -100, -100);
+  triangle(-60, -100, -100, -100, -60, -280);
+  pop();
+  pop();
+
+}
